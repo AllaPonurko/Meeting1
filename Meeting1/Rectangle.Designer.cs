adbd20f73a2240f91e3fda259c8882ee793a58fe
Alla Ponurko <ponurkoalla264@gmail.com>
2022-02-08 04:39:27 +0300

    Добавьте файлы проекта.

---
+++ Meeting1/Rectangle.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Meeting1
+{
+    partial class Rectangle
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Rectangle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Rectangle";
+            this.Text = "Rectangle";
+            this.Load += new System.EventHandler(this.Rectangle_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Rectangle_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Rectangle_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Rectangle_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Rectangle_MouseEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Meeting1/Rectangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meeting1
+{
+    public partial class Rectangle : Form
+    {
+        public Rectangle()
+        {
+            InitializeComponent();
+            this.BackColor=Color.White; 
+        }
+
+        private void Rectangle_Load(object sender, EventArgs e)
+        {
+            
+        }
+        Graphics g;
+        Pen pen = new Pen(Color.Black);
+        private void Rectangle_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(Color.Blue);
+            g.DrawRectangle(pen, 10, 10,Width-40,Height-60);
+        }
+        Point click;
+        private void Rectangle_MouseClick(object sender, MouseEventArgs e)
+        {
+            g = CreateGraphics();
+            click = e.Location;
+            g.DrawEllipse(pen, click.X, click.Y, 300, 100);
+
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseEventArgs e)
+        {
+            click = e.Location;
+            if (e.Button == MouseButtons.Left)
+            {if(click.X==10||click.Y==10||click.X== this.Width - 40
+                    ||click.Y== this.Height - 60)
+                MessageBox.Show("Точка находится на границе прямоугольника");
+                if(click.X < 10 || click.Y < 10 || click.X> this.Width - 40
+                    || click.Y > this.Height - 60)
+                    MessageBox.Show("Точка находится за границей прямоугольника");
+                else { MessageBox.Show("Точка находится внутри прямоугольника"); }
+                
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Ширина окна "+Convert.ToString(this.Size.Width)+
+                    " Высота окна  "+ Convert.ToString(this.Size.Height));
+                
+            }
+        }
+
+        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+             Text = MousePosition.ToString();
+        }
+
+    }
+}
